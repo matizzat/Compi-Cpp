ef465a9d057d6179ac44e1d21926d1eb130b3229
matizzat <mat.torreta@gmail.com>
2024-09-10 09:58:25 -0300

    Add files via upload

---
+++ lotes/lotes-error.c
@@ -0,0 +1,237 @@
+my_variable;
+
+int;
+
+int a
+
+float y 2.8 ;
+
+float y = ;
+
+int i = 3
+
+char arr];
+
+char arr[;
+
+float values []{};
+
+float values []=};
+
+float values []={;
+
+int indexes []={};
+
+int indexes []={4,};
+
+int indexes []={,5};
+
+void func){};
+
+void func({};
+
+void func(int){};
+
+void func(param){};
+
+void func(int &){};
+
+void func(&arr){};
+
+void func(&arr){};
+
+void func(int arr]){};
+
+void func(int arr]){};
+
+void func(int &arr]){};
+
+void func(int &arr[){};
+
+void func(int a, ){};
+
+void func(int a, int){};
+
+void func(int a, b){};
+
+void func(int a, b, int c){};
+
+void func(int &a, int b[, float c){};
+
+void func(int &a, int b], float c){};
+
+void func(int &a[, float b, float c]){};
+
+float func() {
+int ;
+int b = 8;
+}
+
+void func() {
+;int b = 8;
+}
+
+
+void func() {
+b;
+}
+
+void func() {
+int b =;
+}
+
+void func() {
+int =5;
+}
+
+void func() {
+float a=1.0,b=2.4,c=6.3;
+{
+a=a+b;
+}
+
+void func() {
+
+float a=1.0,b=2.4,c=6.3;
+a=a+b;
+}
+}
+
+
+void func() {
+{
+float a=1.0,b=2.4,c=6.3;
+a=a+b;
+}
+}
+
+void func() {
+int i = 0;
+while i<10)
+i=i+1;
+}
+
+void func() {
+int i = 0;
+while(i<10
+i=i+1;
+}
+
+
+void func() {
+int i = 0;
+while(i<10
+i=i+1;
+}
+
+void func() {
+int i = 0;
+while(1<10{
+i=i+1;
+}
+}
+
+void func() {
+int i = 0;
+while i<10){
+i=i+1;
+}
+}
+
+void func() {
+int i = 0;
+while(i<10{
+i=i+1;
+}
+}
+
+void func() {
+int i = 0;
+while i<10){
+i=i+1;
+}
+}
+
+void func() {
+int i = 0;
+while i<10){
+i=i+1;
+}
+}
+
+void func() {
+int i = 0;
+if i<10) a=b;
+}
+
+void func() {
+int i = 0;
+if (i<10 a=b;
+}
+
+
+void func() {
+int i = 0;
+if i<10){
+a=b;
+}
+}
+
+
+void func() {
+int i = 0;
+if i<10){
+a=b;
+}
+}
+
+void func() {
+int i = 0;
+if i<10){
+a=b;
+}
+}
+
+void fmain (){
+int a = 0;
+while(1<2 if (!a) a=1;
+}
+
+void fmain (){
+    int a = 0;
+    while 1<2) 
+        if (!a) a=1;
+}
+
+void fmain (){
+    int a = 0;
+    while (1<2) {
+
+    }
+    else
+    {
+        return 1;
+    } 
+}
+
+void fmain (){
+    cin >> ;
+
+    a = 2*a;
+    cin >> a >> ;
+}
+
+void fmain (){
+    cout << a;
+
+    a = 2*a;
+    cout << << b;
+}
+
+void fmain (){
+    cout << a;
+
+    a = 2*a;
+    cout << << b;
+}
+
---
+++ lotes/lotes-otros.c
@@ -0,0 +1,253 @@
+int a[], b [9], c[]={3,4,5}; char x, y,z ;
+int a[], b [9], c[]=3,4,5;
+int a[], b [9], c[]={3,4,5;
+int a[], b [9], c[]=3,4,5};
+float c 2;
+a=0;
+int =0;
+int a=;
+int a=0
+int a=0 b;
+int a=0,b
+int a=0, b 0;
+int a b,c;
+int a [4;
+int a 9];
+
+main () {}
+int () {}
+int main ) {}
+int main () }
+int main (int a, b) {}
+int main (a, int b) {}
+int main (int a, int & ) {}
+int main (int a,& b) {}
+int main (int a[,float & b) {}
+int main (int a],float & b) {}
+int main (int a[] float & b) {}
+int main () {
+int i=3;
+
+
+int main (int a[], float & b) {
+int b= ;
+}
+
+int main (int a[], float & b) {
+int b 0;
+}
+
+int main (int a[], float & b) {
+int b =0
+}
+
+int main (int a[], float & b) {
+int b =0, p[]=;
+}
+
+int main (int a[], float & b) {
+-b p;
+}
+
+int main (int a[], float & b) {
+-b * p ;
+} 
+
+int main (int a[], float & b) {
+-b * p ||;
+}
+
+int main (int a[], float & b) {
+-b * || o;
+}
+
+int main (int a[], float & b) {
+-9 * || o;
+}
+
+int main (int a[], float & b) {
+|| o;
+}
+
+int main (int a[], float & b) {
+y=9|| 0
+}
+
+int main (int a[], float & b) {
+y=(9|| 0 && 6;
+}
+
+int main (int a[], float & b) {
+!what
+}
+
+/***Proposicion Seleccion***/
+int main (int a[], float & b) {
+if (l==)
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==7
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion a,b))
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion (a, ))
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion ( , b))
+d=0;
+}
+
+int main () {
+if (l==funcion (c , b))
+
+}
+
+int main (int a[], float & b) {
+if (l==funcion (c , b))
+while a<=b)
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion (c , b))
+while ( <=b)
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion (c , b))
+while ( a b)
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion (c , b))
+while ( a<= )
+d=0;
+}
+
+int main (int a[], float & b) {
+if (l==funcion (c , b))
+while ( a<=b
+d=0;
+}
+
+ main (int a[], float & b) {
+if (l==funcion (c , b))
+while ( a<=b) 
+d=0;
+}
+
+ main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+d=0;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return ;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l==;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l 0;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0
+}
+
+/***proposicion e_s***/
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  d;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  >> ;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  >> 
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  << b ;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  <<  ;
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  <<  
+}
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  << d <<; 
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  >> d >>; 
+}
+
+int main () {
+if (l==funcion (c , b))
+while ( a<=b) 
+return l-0;
+cin  >> d >> 
+}
